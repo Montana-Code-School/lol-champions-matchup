14c0aa5b17c321113676d155a1edfcaa3a3df138
Eli Dery <elidery@MacBook-Pro-3.local>
2018-02-20 11:24:19 -0700

    Got the server and compliler working

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
-import express from "express";
+import express from 'express';
 import path from 'path';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+//import API here
 const app = express();
-app.use('/', express.static(path.join(__dirname, "src")));
-//app.use('/static', express.static(path.join(__dirname, 'public')))
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
+const router = express.Router();
+router.use((req, res, next) =>{
+  console.log('I need an adult');
+  next();
+});
+app.use('/', express.static(path.join(__dirname, "dist")));
+
+app.use(bodyParser.json());
 const port= process.env.PORT || 3000;
 
 app.listen(port, () => console.log("Example app listening to port 3000!"));
