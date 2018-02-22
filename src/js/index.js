2cb14dda7155ea1ef29ddff1c9cdded69700bb21
ed42311 <ed42311@gmail.com>
2018-02-21 19:04:52 -0700

    axios rocks

---
+++ src/js/index.js
@@ -0,0 +1,49 @@
+import React, {Component} from 'react';
+import {render} from 'react-dom';
+import axios from 'axios';
+
+const request= "http://ddragon.leagueoflegends.com/cdn/7.15.1/data/en_US/champion.json?api_key=RGAPI-3ff47e8c-88b0-4f8d-a108-280272dd665d";
+
+class SearchButton extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      name: "",
+      champion: {},
+      allChamps: {},
+      search: "",
+      champNames: []
+    };
+  }
+
+  onNameChange(e) {
+    let valueLower = String(e.target.value).toLowerCase();
+    let champNames = Object.keys(this.state.allChamps);
+    champNames = champNames
+      .map(name => name.toLowerCase())
+      .filter((name => name.includes(valueLower)));
+      this.setState({
+        champNames: champNames
+      });
+  }
+
+  componentWillMount() {
+    axios.get(request).then((data) =>{
+      this.setState({
+        allChamps: data.data.data
+      });
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <input id= "inputText" type= "text" name= "name" onChange={(e) => this.onNameChange(e)}></input>
+        <button className="inputs" onClick={() => this.onButtonClick()}>Search Champions</button>
+        {this.state.champNames.map(name => <p>{name}</p>)}
+      </div>
+    );
+  }
+}
+
+render(<SearchButton/>, document.getElementById('ChampBox'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,48 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const paths = {
+  DIST: path.resolve(__dirname, 'dist'),
+  SRC: path.resolve(__dirname, 'src'),
+  JS: path.resolve(__dirname, 'src/js'),
+};
+
 module.exports = {
-  entry: './src/index.js',
+  entry: path.join(paths.JS, 'index.js'),
   output: {
-    filename: './dist/bundle.js'
+    path: paths.DIST,
+    filename: 'bundle.js',
+    publicPath: '/'
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(paths.SRC, 'index.html'),
+    }),
+  ],
   module: {
-    loaders: [
+    rules: [
       {
-        test: /.jsx?$/,
-        loader: 'babel-loader',
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        query: {
-          presets: ['env', 'react']
-        }
-      }
-    ]
+        use: [
+          'babel-loader',
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader' ]
+      },
+    ],
+  },
+  devServer: {
+    historyApiFallback: true,
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+    modules:['node_modules', 'src'],
+    alias: {
+      css: path.resolve(__dirname, 'src/css/'),
+      comp: path.resolve(__dirname, 'src/js/Components')
+    }
   },
 };
